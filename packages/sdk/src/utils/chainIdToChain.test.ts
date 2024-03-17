92719787f6526eeca9b2ec6c37ce83f7514349fe
jvaleskadevs <vmjune2019@gmail.com>
2024-03-17 02:21:04 +0100

    addChainIdToChainTest

--- packages/sdk/src/utils/chainIdToChain.test.ts
+++ packages/sdk/src/utils/chainIdToChain.test.ts
@@ -8,11 +8,17 @@ test("chainIdToChain", () => {
   const polygonChain = chainIdToChain(137)
   const mumbaiChain = chainIdToChain(80001)
   const sepoliaChain = chainIdToChain(11155111)
+  const baseSepoliaChain = chainIdToChain(84532)
+  const optimismSepoliaChain = chainIdToChain(11155420)
+  const zoraSepoliaChain = chainIdToChain(999999999)
 
   expect(mainnetChain.id).toEqual(1)
   expect(goerliChain.id).toEqual(5)
   expect(polygonChain.id).toEqual(137)
   expect(mumbaiChain.id).toEqual(80001)
   expect(sepoliaChain.id).toEqual(11155111)
-    
+  expect(baseSepoliaChain.id).toEqual(84532)
+  expect(optimismSepoliaChain.id).toEqual(11155420)
+  expect(zoraSepoliaChain.id).toEqual(999999999)
+  
 })
